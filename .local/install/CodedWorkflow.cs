dba38e1a20ad4a94f9da2093b4f03491f4b20f50
haind96 <nguyendinhhai196@gmail.com>
2025-03-12 22:12:11 +0700

    cm

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace RoboticEnterpriseFramework
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
